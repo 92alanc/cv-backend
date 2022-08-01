3b0a62dbc53e8a018e79bfb9d671a31ba8afac5a
Alan Camargo <alan.uk.br@gmail.com>
2022-08-01 20:26:06 +0100

    Update spring

--- src/main/java/com/alancamargo/cvbackend/controller/ContactInfoController.java
+++ src/main/java/com/alancamargo/cvbackend/controller/ContactInfoController.java
@@ -3,25 +3,21 @@ package com.alancamargo.cvbackend.controller;
 import com.alancamargo.cvbackend.model.ContactInfo;
 import com.alancamargo.cvbackend.service.ContactInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @SuppressWarnings("unused")
-@RestController
+//@RestController
 public class ContactInfoController {
 
     @Autowired
     private ContactInfoService contactInfoService;
 
-    @GetMapping("/contact-info")
+    //@GetMapping("/contact-info")
     public ContactInfo getContactInfo() {
         return contactInfoService.getContactInfo();
     }
 
-    @PostMapping("/contact-info")
-    public void updateContactInfo(@RequestBody ContactInfo contactInfo) {
+    //@PostMapping("/contact-info")
+    public void updateContactInfo(/*@RequestBody*/ ContactInfo contactInfo) {
         contactInfoService.updateContactInfo(contactInfo);
     }
 
--- src/main/java/com/alancamargo/cvbackend/controller/JobController.java
+++ src/main/java/com/alancamargo/cvbackend/controller/JobController.java
@@ -3,24 +3,22 @@ package com.alancamargo.cvbackend.controller;
 import com.alancamargo.cvbackend.model.Job;
 import com.alancamargo.cvbackend.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @SuppressWarnings("unused")
-@RestController
+//@RestController
 public class JobController {
 
     @Autowired
     private JobService jobService;
 
-    @GetMapping("/jobs/current")
+    //@GetMapping("/jobs/current")
     public Job getCurrentJob() {
         return jobService.getCurrentJob();
     }
 
-    @GetMapping("/jobs/previous")
+    //@GetMapping("/jobs/previous")
     public List<Job> getPreviousJobs() {
         return jobService.getPreviousJobs();
     }
